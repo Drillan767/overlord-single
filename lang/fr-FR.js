1a63975eaa2a1a97e85cdaa7ab816ac6d0027b21
Joseph Levarato <jlevarato@pm.me>
2021-03-29 00:00:01 +0200

    Woops. Now in 4k.

--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -1,6 +1,6 @@
 export default {
   subtitle: 'Ce site est actuellement en construction, et sera bien entendu un peu plus complexe que celui ci.',
-  info: "Dans tous les cas, vous pouvez me joindre via les réseau ci-dessous, ou m'envoyer un mail !",
+  info: "Dans tous les cas, vous pouvez me joindre via les réseaux ci-dessous, ou m'envoyer un mail !",
   email: {
     name: 'Nom',
     subject: 'Objet',
